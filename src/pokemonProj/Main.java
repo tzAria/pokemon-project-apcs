fbad0876aeff58bd274ed3c10aa130009a6369ac
Theon Zegers <theon.zegers@stu.elps.us>
2019-01-11 08:00:27 -0500

    Initial commit

---
+++ src/pokemonProj/Main.java
@@ -0,0 +1,72 @@
+package pokemonProj;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		//create all pokemon: swinub, cleffa, azurill, pineco, pansear
+		Pokemon swinub = new Pokemon(220, "Swinub", 50, 50, 40, 30, 30, 50, 250, false, false, false);
+		Pokemon cleffa = new Pokemon(173, "Cleffa", 50, 25, 28, 45, 55, 15, 218, false, false, false);
+		Pokemon azurill = new Pokemon(298, "Azurill", 50, 20, 40, 20, 40, 20, 190, false, false, false);
+		Pokemon pineco = new Pokemon(204, "Pineco", 50, 65, 90, 35, 35, 15, 290, false, false, false);
+		Pokemon pansear = new Pokemon(513, "Pansear", 50, 53, 48, 53, 48, 64, 316, false, false, false);
+		ArrayList<Pokemon> pokemonList = new ArrayList<Pokemon>();
+		ArrayList<Pokemon> activeList = new ArrayList<Pokemon>();
+		pokemonList.add(swinub);
+		pokemonList.add(cleffa);
+		pokemonList.add(azurill);
+		pokemonList.add(pineco);
+		pokemonList.add(pansear);
+		System.out.println("Battle start! For extra atmosphere please open this link in the background: https://www.youtube.com/watch?v=bIlomr9Guas");
+		Scanner console = new Scanner(System.in);
+		System.out.println("Which pokemon do you choose? Your choices are: "  + swinub.getName() + ", " + cleffa.getName() + ", " + azurill.getName() + ", " + pineco.getName() + ", " + pansear.getName());
+		String playerPokemonChoice = console.nextLine();
+		if(playerPokemonChoice.equals("Swinub") || playerPokemonChoice.equals("swinub")) {
+			pokemonList.remove(swinub);
+			activeList.add(swinub);
+			swinub.setActive();
+		}
+		else if(playerPokemonChoice.equals("Cleffa") || playerPokemonChoice.equals("cleffa")) {
+			pokemonList.remove(cleffa);
+			activeList.add(cleffa);
+			cleffa.setActive();
+		}
+		else if(playerPokemonChoice.equals("Azurill") || playerPokemonChoice.equals("azurill")) {
+			pokemonList.remove(azurill);
+			activeList.add(azurill);
+			azurill.setActive();
+		}
+		else if(playerPokemonChoice.equals("Pineco") || playerPokemonChoice.equals("pineco")) {
+			pokemonList.remove(pineco);
+			activeList.add(pineco);
+			pineco.setActive();
+		}
+		else if(playerPokemonChoice.equals("Pansear") || playerPokemonChoice.equals("pansear")) {
+			pokemonList.remove(pansear);
+			activeList.add(pansear);
+			pansear.setActive();
+		}
+		int range = (4);     
+	    int cpuChoiceIndex = (int)(Math.random() * range) + 0;
+	    System.out.println(cpuChoiceIndex);
+	    if(cpuChoiceIndex == 0) {
+	    	pokemonList.get(0).setCPUActive();
+	    	System.out.println("CPU has chosen " + pokemonList.get(0).getName());
+	    	pokemonList.remove(0);
+	    }
+	    else if(cpuChoiceIndex == 1) {
+	    	pokemonList.get(1).setCPUActive();
+	    	System.out.println("CPU has chosen " + pokemonList.get(1).getName());
+	    	pokemonList.remove(1);
+	    }
+	    else if (cpuChoiceIndex == 2) {
+	    	pokemonList.get(2).setCPUActive();
+	    	System.out.println("CPU has chosen " + pokemonList.get(2).getName());
+	    	pokemonList.remove(2);
+	    }
+	    else if (cpuChoiceIndex == 3) {
+	    	pokemonList.get(3).setCPUActive();
+	    	System.out.println("CPU has chosen " + pokemonList.get(3).getName());
+	    	pokemonList.remove(3);
+	    }
+	}
+}
---
+++ src/pokemonProj/Pokemon.java
@@ -0,0 +1,87 @@
+package pokemonProj;
+import java.util.*;
+public class Pokemon {
+	private int id;
+	private String name;
+	private int hp;
+	private int attack;
+	private int def;
+	private int spAtt;
+	private int spDef;
+	private int speed;
+	private int totalStats;
+	public boolean isFaint;
+	public boolean isActive;
+	public boolean isCPU;
+	public Pokemon(int id, String name, int hp, int attack, int def, int spAtt, int spDef, int speed, int totalStats, boolean isFaint, boolean isActive, boolean isCPU) {
+		this.id = id;
+		this.name = name;
+		this.hp = hp;
+		this.attack = attack;
+		this.def = def;
+		this.spAtt = spAtt;
+		this.spDef = spDef;
+		this.speed = speed;
+		this.totalStats = totalStats;
+		this.isFaint = isFaint;
+		this.isActive = isActive;
+		this.isCPU = isCPU;
+	}
+	public String getName() {
+		return name;
+	}
+	public int giveDamage(int attack, int defense, int totalStats) {
+		double n = (Math.random() * 1.85 + 0.85);
+		int damage = (int) ((((attack / defense) * totalStats) / (250) + 2) * n);
+		return damage;
+	}
+	public int giveSpecialDamage(int spAtt, int spDef, int totalStats) {
+		double n = (Math.random() * 1.85 + 0.85);
+		int specDamage = (int) ((((spAtt / spDef) * totalStats) / (250) + 5) * n);
+		return specDamage;
+	}
+	public int[] changeStatsNormal(int hp, int attack, int defense) {
+		int[] statsAfter = {hp += 2, attack += 2, defense += 2};
+		return statsAfter;
+	}
+	public int[] changeStatsSpecialSubtraction(int spAtt, int spDef) {
+		int[] specStatsAfter = {spAtt -= 2, spDef -= 2};
+		return specStatsAfter;
+	}
+	public int changeStatsSpecialAddition(int hp) {
+		int hpAfter = hp += 4;
+		return hpAfter;
+	}
+	public int takeDamage(int damage, int hp) {
+		return hp - damage;
+	}
+	public String useMove(String name) {
+		if (name.equals("Swinub")) {
+			return "Ice Beam";
+		}
+		else if (name.equals("Cleffa")) {
+			return "Moonblast";
+		}
+		else if (name.equals("Azurill")) {
+			return "Water Gun";
+		}
+		else if (name.equals("Pineco")) {
+			return "Razor Leaf";
+		}
+		else if (name.equals("Pansear")) {
+			return "Ember";
+		}
+		else {
+			return "404";
+		}
+	}
+	public void setActive() {
+		isActive = true;
+	}
+	public void setCPUActive() {
+		isCPU = true;
+	}
+	public void setInActive() {
+		isActive = false;
+	}
+}
